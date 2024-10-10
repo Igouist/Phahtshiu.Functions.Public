e3cba54d6bfbcbfb8e1db07c910f44f583361735
Igouist <Ursaliker@gmail.com>
2024-10-10 17:32:40 +0800

    [Feat] 新增 GitHub Webhook 入口

---
+++ Phahtshiu.Functions/Endpoints/GithubEndpoints.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace Phahtshiu.Functions.Endpoints;
+
+public class GithubEndpoints
+{
+    private readonly IMediator _mediator;
+
+    public GithubEndpoints(
+        IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    /// <summary>
+    /// 接收 Github Webhook 訊息
+    /// </summary>
+    /// <returns></returns>
+    [Function("Github-Webhook")]
+    public async Task<string> ReceiveGithubWebhook(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] 
+        HttpRequestData req)
+    {
+        var body = await new StreamReader(req.Body).ReadToEndAsync();
+        return body;
+    }
+}
