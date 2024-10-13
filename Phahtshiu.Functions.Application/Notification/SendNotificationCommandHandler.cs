f848518cb55e308cacf7fbc440bcdc5ecb5722c3
Igouist <Ursaliker@gmail.com>
2024-10-13 17:46:34 +0800

    Revert "Revert "[Feat] 加入訂便當定時提醒""
    This reverts commit 0b13b60953d21954e1348a00e423e7bb749aeb1e.

---
+++ Phahtshiu.Functions.Application/Notification/SendNotificationCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Phahtshiu.Functions.Application.Notification.Models;
+using Phahtshiu.Functions.Application.Notification.Services;
+
+namespace Phahtshiu.Functions.Application.Notification;
+
+/// <summary>
+/// 發送通知
+/// </summary>
+public record SendNotificationCommand(
+    string Title,
+    string Message,
+    string? Url = null,
+    string? Group = null) : IRequest;
+
+public class SendNotificationCommandHandler : IRequestHandler<SendNotificationCommand>
+{
+    private readonly INotificationService _notificationService;
+
+    public SendNotificationCommandHandler(
+        INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+    
+    public Task Handle(
+        SendNotificationCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var message = new NotificationBody
+        {
+            Title = request.Title,
+            Message = request.Message,
+            Url = request.Url!,
+            Group = request.Group!
+        };
+        
+        return _notificationService.NotificationAsync(message);
+    }
+}
