3134107dd4ff44dbbe6d0642a95bac5f11db8d7e
Igouist <Ursaliker@gmail.com>
2026-03-11 22:17:40 +0800

    fix: 更新天氣預報通知中的連結至正確的氣象局網址

--- Phahtshiu.Functions.Application/UseCases/Reminder/SendWeatherForecastReminderCommand.cs
+++ Phahtshiu.Functions.Application/UseCases/Reminder/SendWeatherForecastReminderCommand.cs
@@ -37,7 +37,7 @@ public class SendWeatherForecastReminderCommandHandler(
             Title = $"[天氣] {forecast.LocationName} 今日天氣預報",
             Message = $"{forecast.WeatherDescription}｜{forecast.MinTemperature}°C ~ {forecast.MaxTemperature}°C｜降雨機率 {forecast.RainProbability}%",
             Group = "Weather",
-            Url = "https://www.cwb.gov.tw/V8/C/W/County/index.html"
+            Url = "https://www.cwa.gov.tw/V8/C/"
         };
 
         await notificationService.NotificationAsync(message);
